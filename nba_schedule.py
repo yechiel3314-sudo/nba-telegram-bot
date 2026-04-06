140aa583b38fd4ebad20a10f4512206a36d39938
miki <mikibinet@gmail.com>
2026-04-06 17:22:44 +0300

    Update nba_schedule.py

--- nba_schedule.py
+++ nba_schedule.py
@@ -12,7 +12,7 @@ TELEGRAM_TOKEN = "8514837332:AAFZmYxXJS43Dpz2x-1rM_Glpske3OxTJrE"
 CHAT_ID = "-1003808107418"
 
 # זמן שליחה מתוכנן (ניתן לשנות לצורך בדיקה)
-SCHEDULE_TIME_STR = "17:20"
+SCHEDULE_TIME_STR = "17:23"
 
 ESPN_API_URL = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard"
 RTL_MARK = "\u200f"
@@ -23,6 +23,11 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
+
+def log(msg):
+    tz = pytz.timezone("Asia/Jerusalem")
+    now = datetime.now(tz).strftime("%H:%M:%S")
+    print(f"[{now}] {msg}")
 
 # ==============================================================================
 # --- מילון תרגום קבוצות NBA ---
