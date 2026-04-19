a31b6827436fca9d67387005fc9a7738fd75bc97
miki <mikibinet@gmail.com>
2026-04-19 20:41:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1046,7 +1046,7 @@ def run():
         except Exception as e:
             print(f"❌ שגיאה כללית בלולאה: {e}")
 
-        time.sleep(5)
+        time.sleep(10)
 
 if __name__ == "__main__":
     run()
