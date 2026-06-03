c52e288ce6b4043f17eb08f49b6296fa9055a0e5
miki <mikibinet@gmail.com>
2026-06-03 13:44:50 +0300

    Update nba.py

--- nba.py
+++ nba.py
@@ -9,9 +9,17 @@ from datetime import datetime
 # הגדרות
 # ==========================================
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "8996455073:AAHXYXjy2T12CzBi-IqramkUSWQ4rDSI6ss")
-CHAT_ID = os.getenv("CHAT_ID", "-1003808107418")
+import os
 
+TELEGRAM_TOKEN = os.environ.get(
+    "NBA_LIVE_TELEGRAM_BOT_TOKEN_PRIVATE",
+    ""
+).strip()
+
+CHAT_ID = os.environ.get(
+    "NBA_LIVE_TELEGRAM_CHAT_ID_PRIVATE",
+    ""
+).strip()
 # ה-API המרכזי לתוצאות חיות
 NBA_URL = "https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json"
 
